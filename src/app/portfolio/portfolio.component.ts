4864a988d110f1994cd28d0dfcc83428d116bcfa
marcin.cyboran <marcin.cyboran.wsb@gmail.com>
2018-05-31 13:27:20 +0200

    #portfolio #mixitup

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy
+} from '@angular/core';
+import mixitup from 'mixitup';
+import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent implements OnInit {
-
-  constructor() { }
+export class PortfolioComponent implements OnInit, OnDestroy {
+  @ViewChild('test') wrapper: ElementRef;
+  mix: any;
+  constructor() {}
 
   ngOnInit() {
+    this.mixItUpInit();
   }
 
+  ngOnDestroy() {
+    // Reset is needed else it won't work after changing components
+    this.mix.destroy();
+  }
+
+  mixItUpInit() {
+    this.mix = mixitup('#mixitup-wrapper', {
+      load: {
+        sort: 'year:random' /* default:asc */
+      },
+      animation: {
+        effects: 'fade scale',
+        duration: 700
+      },
+      classNames: {
+        block: 'programs',
+        elementFilter: 'filter-btn',
+        elementSort: 'sort-btn'
+      },
+      selectors: {
+        target: '.mixitup-target'
+      }
+    });
+  }
 }
